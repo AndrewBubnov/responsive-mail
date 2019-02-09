ed4df25924fc28a6407e25482e11d81ea1ff4d27
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-02-09 22:17:58 +0200

    Updates

---
+++ precache-manifest.88dcb508524963ac99c18c26b721872e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1698fab7382cb81b9553013f0932409d",
+    "url": "/responsive-mail/static/media/trashcan.1698fab7.svg"
+  },
+  {
+    "revision": "c6ef976861cca9a9339b160b1e5dd352",
+    "url": "/responsive-mail/static/media/recycle1.c6ef9768.svg"
+  },
+  {
+    "revision": "93dbe78a218faef6179a",
+    "url": "/responsive-mail/static/js/runtime~main.93dbe78a.js"
+  },
+  {
+    "revision": "9c21b8a09978876a7040",
+    "url": "/responsive-mail/static/js/main.9c21b8a0.chunk.js"
+  },
+  {
+    "revision": "06dd3dda81e1c95d11cb",
+    "url": "/responsive-mail/static/js/1.06dd3dda.chunk.js"
+  },
+  {
+    "revision": "9c21b8a09978876a7040",
+    "url": "/responsive-mail/static/css/main.3744b7c7.chunk.css"
+  },
+  {
+    "revision": "5c51aca0be1caa4b1b64af98b738a311",
+    "url": "/responsive-mail/index.html"
+  }
+];
